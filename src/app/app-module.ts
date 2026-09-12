ea76c70df86f8ba26973f37ee3fe60334224e4d4
Isair Valdivia <85393@alumnos.utleon.edu.mx>
2026-09-11 18:55:44 -0600

    Form Module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,11 +2,12 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import {FormsModule} from '@angular/forms';
 import { List } from './heroes/list/list';
 
 @NgModule({
   declarations: [App, List],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+templateUrl:'/app.html'
+,
   standalone: false,
   styleUrl: './app.css',
-  templateUrl: './app.html',
+  
 })
+
 export class App {
 
  title:string = 'Edgar Isair Valdivia López'
